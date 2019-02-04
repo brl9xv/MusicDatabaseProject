9bc837fcd5c76a6be1327af43f6f8c838f69df2b
Acharne <brl9xv@mst.edu>
2019-02-04 09:24:13 -0600

    Added basic music controls

--- ia.py
+++ ia.py
@@ -9,14 +9,32 @@ import vlc
 
 class music:
     def __init__(self, bot):
+        # Save reference to passed bot instance
         self.bot = bot
+        
+        # Connect to database and create operation cursor
         self.con = psycopg2.connect(database='musicdatabase',user='gigacorn',password='bplieb123',host='localhost',port='5432')
         self.cur=self.con.cursor()
+
+        # Set up audio task loop
+        self.bot.create_task(self.audio_loop())
+        self.player = vlc.Instance().media_player_new()
+        self.player.event_manager().event_attach(vlc.EventType.MediaPlayerEndReached,self.song_finished)
+        self.queue = asyncio.Queue()
+        self.queue_list = []
+        self.next = False
+        self.playing = False
+
+        # Save youtube-dl options
         self.opts={
                 'format':'bestaudio',
                 'outtmpl':'',
                 'postprocessors':[{'key':'FFmpegExtractAudio','preferredcodec':'m4a'}],
                 }
+
+    def song_finished(self, event):
+        if event.type == vlc.EventType.MediaPlayerEndReached:
+            self.next = True
 
     async def download(self, key):
         try:
@@ -26,6 +44,68 @@ class music:
         except(...):
             print('Download error caught...')
             print(e)
+
+    async def audio_loop(self):
+        while True:
+            self.next = False
+            media_path = await self.queue.get()
+            if not os.path.isfile(media_path):
+                print('Media does not exist')
+            else:
+                self.player.set_media(vlc.Instance().media_new_path(media_path))
+                self.player.play()
+                self.playing = True
+                while not self.next:
+                    await asyncio.sleep(1)
+                self.player.stop()
+                self.playing = False
+
+    async def searchdb(self, content):
+        cut = content.find(' ')
+        param = content[:cut]
+        term = content[cut+1:]
+        if not (param == "title" or param == "artist" or param == "genre"):
+            param = "title"
+            term = content
+        self.cur.execute("select title, artist, genre, link from music where {0} like '%{1}%';".format(param,term))
+        return self.cur.fetchall()
+
+    @commands.command(pass_context=True, no_pm=True)
+    async def play(self, ctx):
+        try:
+            results = await self.searchdb(ctx.message.content[9:])
+            if len(results) < 1:
+                await self.bot.send_message(ctx.message.channel,'No results...')
+            elif len(results) == 1:
+                await self.bot.send_message(ctx.message.channel,'Enqueing: '+results[0][0]+" - "+results[0][1])
+                await self.queue.put("../Music/"+results[0][3]+".m4a")
+            else:
+                await self.bot.send_message(ctx.message.channel,'Multiple results, pick a number or all...')
+                all_results = ''
+                for i in range(len(results)):
+                    all_results += str(i+1)+': '+results[i][0]+' - '+results[i][1]+'\n'
+                await self.bot.send_message(ctx.message.channel, all_results)
+                choice_m = await self.bot.wait_for_message(author=ctx.message.author)
+                choice = choice_m.content
+                if choice == 'all':
+                    await self.bot.send_message(ctx.message.channel,'Enqueing: all results')
+                    for r in results:
+                        await self.queue.put("../Music/"+r[3]+".m4a")
+                else:
+                    await self.bot.send_message(ctx.message.channel,'Enqueing: '+results[int(choice)-1][0])
+                    await self.queue.put("../Music/"+results[int(choice)-1][3]+".m4a")
+
+        except psycopg2.Error as e:
+            await self.bot.send_message(ctx.message.channel,'Error...')
+            print(e)
+
+    @commands.command(pass_context=True, no_pm=True)
+    async def skip(self, ctx):
+        if self.playing:
+            await self.bot.send_message(ctx.message.channel,'Skipping...')
+            self.next = True
+        else:
+            await self.bot.send_message(ctx.message.channel,'Nothing is playing...')
 
     
     @commands.command(pass_context=True, no_pm=True)
